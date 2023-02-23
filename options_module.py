e6046518d7e2be95c3d445c1f222e36013656671
João Jungblut <99040917+JoaoJungblut@users.noreply.github.com>
2023-02-23 13:25:56 -0300

    Update in options_module
    we create two functions to calculate the price variation in continues time of an option. The first one receive a pandas series and calculate the return, while the second function receive a pandas series and calculate the cumulative return. The output is type of pandas Series. Both functions were constructed to handle with exceptions like TypeError, ValueError and AttributeError.

--- options_module.py
+++ options_module.py
@@ -1,8 +1,42 @@
-#import pandas as pd
-#import numpy as np
+# Joao and Matheus: 2023-02-17
+# function to calculate the return of a call or put
 
-def calculate_options_return(price_series: pd.core.series.Series) -> pd.core.series.Series:
-    pass
+import pandas as pd
+import numpy as np
+import warnings 
 
-def calculate_cumulative_options_return(price_series: pd.core.series.Series) -> pd.core.series.Series:
-    pass
+def get_options_return(price_series: pd.core.series.Series) -> pd.core.series.Series:
+    
+    """
+    This function calculate the return (percentage variation) of a prices series of stock options
+
+    Args:
+        price_series: a pandas Series dtype float64
+    """
+    
+    try:
+        return_series = np.log(price_series).diff().fillna(0)
+    except (TypeError, AttributeError, ValueError):
+        return_series = -1
+        warnings.warn("Input must be a pandas.core.series.Series dtype float64.")
+    
+    return return_series
+
+
+def get_options_cumreturn(price_series: pd.core.series.Series) -> pd.core.series.Series:
+    
+    """
+    This function calculate the cumulative return (percentage variation) of a prices series of stock options
+
+    Args:
+        price_series: a pandas Series dtype float64
+    """
+    
+    try:
+        return_series = np.log(price_series).diff().fillna(0)
+        cum_return = np.exp(np.cumsum(return_series)) - 1
+    except (TypeError, AttributeError, ValueError):
+        cum_return = -1
+        warnings.warn("Input must be a pandas.core.series.Series dtype float64.")
+    
+    return cum_return
