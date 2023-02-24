c685226a9012984713e4fd7e3cf099af9731560b
gp3try <120216105+gp3try@users.noreply.github.com>
2023-02-23 22:23:00 -0300

    Update get_fin_data.py
    Coloquei duas funções, uma para retornar somente o preço de fechamento ajustado de um ativo no formato pandas.core.series.Series e outra para retornar as informações de preços de abertura, máximo, mínimo, fechamento e fechamento ajustado e do volume transacionado.
    
    Não entendi aquela função de clean_data

--- get_fin_data.py
+++ get_fin_data.py
@@ -1,12 +1,46 @@
-#import yfinance
-#import datetime
-#import pandas
+#Gustavo Pery
+# function to download financial information about a given asset
 
-def get_stock(ticker: str, start_data: datetime.datetime, end_date: datetime.datetime) -> pandas.DataFrame:
-    pass
+import yfinance as yf
+import pandas as pd
 
-def clean_data(data: pandas.DataFrame) -> pandas.DataFrame:
-    pass
+def get_stock() -> pd.DataFrame:
+    
+    """
+    This function will generate financial information (such as prices - Open, High, Low, Close and Adj Close - and transactions volume) about a given asset
+    in a pandas df.
+    Args:
+        
+    """
+    ticker = str(input('\nB3 stock code'))
+    initial_date = str(input('\nInicial date format as YYYY-MM-DD'))
+    final_date = str(input('\nFinal date format as YYYY-MM-DD'))
+    interval = str(input('\nChoose an interval: 1h, 1d, 5d, 1wk, 1mo, 3mo'))
 
-def get_data_price(data: pandas.DataFrame) -> pd.core.series.Series:
-    pass
+    prices = pd.DataFrame(yf.download(ticker+'.SA', initial_date, final_date, interval=interval))
+    prices['Ticker'] = ticker
+
+    return prices
+    
+#def clean_data(data: pandas.DataFrame) -> pandas.DataFrame:
+#    pass
+#
+#               Isso aqui eu não entendi muito bem, é pre limpar a memória?
+
+
+def get_data_price() -> pd.core.series.Series:
+        
+    """
+    This function will generate an adjusted closing prices in a pandas series of a given asset.
+    Args:
+        
+    """
+    ticker = str(input('\nB3 stock code'))
+    initial_date = str(input('\nInicial date format as YYYY-MM-DD'))
+    final_date = str(input('\nFinal date format as YYYY-MM-DD'))
+    interval = str(input('\nChoose an interval: 1h, 1d, 5d, 1wk, 1mo, 3mo'))
+
+    prices = pd.Series(yf.download(ticker+'.SA', initial_date, final_date, interval=interval)['Adj Close'])
+    
+
+    return prices
