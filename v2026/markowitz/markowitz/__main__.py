b15584fb67d30f5617816d4f4e10d587dd176840
felipealmsz <142940807+felipealmsz@users.noreply.github.com>
2026-09-15 12:00:40 -0300

    Enhance project description in __main__.py
    Updated project description to include optimal portfolio calculation.

--- v2026/markowitz/markowitz/__main__.py
+++ v2026/markowitz/markowitz/__main__.py
@@ -1,6 +1,6 @@
 """
-Nome do Projeto: Projeto de aplicação do modelo Markowitz
-Descrição: O projeto calcula a carteira 
+Projeto de aplicação do modelo Markowitz
+O projeto calcula a carteira ótima com base no modelo Markowitz (1952)
 Autor: 
 Data de Criação: 2026-09-15
 Versão: 1.0.0
