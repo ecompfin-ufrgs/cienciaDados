e9cc606b4406cd050c8b4f4810717a90ddc4043c
franco-mendes-1994 <155776520+franco-mendes-1994@users.noreply.github.com>
2026-09-15 11:56:12 -0300

    Update __main__.py

--- v2026/capm/capm/__main__.py
+++ v2026/capm/capm/__main__.py
@@ -1,5 +1,5 @@
 # Implementação CAPM
 ## Descrição: Este é o arquivo main do projeto de implementação do CAPM na disciplina de Ciência de Dados Aplicada à Finanças
-## Autores: Franco Mendes e Rachel Adolpho
+## Autores: Franco Mendes
 ## Data: 15/09/2026
 ## Versão: 0.0.1
